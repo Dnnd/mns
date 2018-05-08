e28439fb207806f0d6ba7c7bd491d33de38996ab
Dnnd <ddann9@live.com>
2018-05-08 19:21:06 +0300

    l3+, l2+

--- lab2/grad descent.py
+++ lab2/grad descent.py
@@ -1,23 +1,27 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[109]:
 
 
 import numpy as np
 
+
 def sigmoid(x):
     return 1 / (np.exp(-x) + 1)
 
-def derivative(y,x,w, i):    
+
+def derivative(y, x, w, i):
     ret = -y * x[i] * sigmoid(-y * np.dot(x, w))
     return ret
 
-def logistic_loss(x, y,w):
-    return np.sum(np.log( 1 + np.exp(-y * np.dot(x,w))))
+
+def logistic_loss(x, y, w):
+    return np.sum(np.log(1 + np.exp(-y * np.dot(x, w))))
+
 
 class GradDescenter:
-    
+
     def __init__(self, x, y, w, dimx):
         self.x = np.array(x)
         self.y = np.array(y)
@@ -25,23 +29,22 @@ class GradDescenter:
         self.n = 0
         self.order = len(y)
         self.dimx = dimx
-    
-       
+
     def step(self, n):
-        next_w = self.w -  1/np.sqrt(n + 1) * self.grad()
-        eps = np.linalg.norm(next_w - self.w)   
+        next_w = self.w - 1/np.sqrt(n + 1) * self.grad()
+        eps = np.linalg.norm(next_w - self.w)
         self.w = next_w
         return eps
-    
-    def grad(self):        
+
+    def grad(self):
         return np.array([self.part_deriv(i) for i in range(0, self.dimx)])
-        
-    
-    def part_deriv(self, i):        
-        terms = [derivative(self.y[k], self.x[k], self.w, i) for k in range(0, self.order)]                
+
+    def part_deriv(self, i):
+        terms = [derivative(self.y[k], self.x[k], self.w, i)
+                 for k in range(0, self.order)]
         res = np.sum(terms)
         return res
-    
+
     def train(self, eps):
         err = np.inf
         n = 0
@@ -49,74 +52,75 @@ class GradDescenter:
             err = self.step(n)
             n = n + 1
         return n
-    
+
     def validate(self, x, y):
         return logistic_loss(x, y, self.w)
-    
-
-
-# In[2]:
 
 
 class LdaClassifier:
-         
-    def __init__(self,T, train_set):
+
+    def __init__(self, T, train_set):
         self.train_set = train_set
-        self.T = T        
+        self.T = T
         self._cov = None
         self._m1 = None
-        self._m2 = None        
-    
-    def train(self):  
-        
-        m0, m1 = self.get_mean()        
-        cov = self.get_cov()   
-        
+        self._m2 = None
+
+    def train(self):
+
+        m0, m1 = self.get_mean()
+        cov = self.get_cov()
+
         cov_inv = np.linalg.inv(cov)
         self.w = np.linalg.inv(cov)*(m0 - m1)
-        self.c = 0.5 * (self.T - np.transpose(m0) * cov_inv * m0 + np.transpose(m1) * cov_inv * m1)  
- 
-    def get_cov(self):        
+        self.c = 0.5 * (self.T - np.transpose(m0) * cov_inv *
+                        m0 + np.transpose(m1) * cov_inv * m1)
+
+    def get_cov(self):
         if self._cov is not None:
             return self._cov
-        
+
         n1 = len(train_set[0]['y'])
         n2 = len(train_set[1]['y'])
-        
+
         x1 = [np.transpose(np.matrix(x)) for x in train_set[0]['x']]
         x2 = [np.transpose(np.matrix(x)) for x in train_set[1]['x']]
-        
-        m1,m2 = self.get_mean()
-   
-        s1 = 1/n1 * np.sum( [ (x - m1) * np.transpose(x - m1) for x in x1], axis=0)
-        s2 = 1/n2 * np.sum([ (x - m2) * np.transpose(x - m2) for x in x2],axis=0)
 
-        self._cov = n1/(n1 + n2) * s1 + n2/(n1 + n2) * s2                
+        m1, m2 = self.get_mean()
+
+        s1 = 1/n1 * np.sum([(x - m1) * np.transpose(x - m1)
+                            for x in x1], axis=0)
+        s2 = 1/n2 * np.sum([(x - m2) * np.transpose(x - m2)
+                            for x in x2], axis=0)
+
+        self._cov = n1/(n1 + n2) * s1 + n2/(n1 + n2) * s2
         return self._cov
-      
-    def get_mean(self):        
+
+    def get_mean(self):
         if self._m1 is not None and self._m2 is not None:
             return self._m1, self._m2
-        
+
         x1 = train_set[0]['x']
         x2 = train_set[1]['x']
-    
+
         n1 = len(x1)
-        n2 = len(x2)                                   
-        
+        n2 = len(x2)
+
         m1 = 1/n1 * np.sum(x1, axis=0)
-        m2 = 1/n2 * np.sum(x2, axis=0)       
-             
+        m2 = 1/n2 * np.sum(x2, axis=0)
+
         self._m1 = np.transpose(np.matrix(m1))
         self._m2 = np.transpose(np.matrix(m2))
-                            
+
         return self._m1, self._m2
-        
-        
-    
+
+    def validate(self, x, y):
+        w = np.concatenate(([np.squeeze(np.asarray(self.c))],
+                            np.squeeze(np.asarray(self.w))), axis=0)
+        return logistic_loss(x, y, w)
 
 
-# In[8]:
+# In[156]:
 
 
 from numpy import random as rnd
@@ -125,67 +129,70 @@ import numpy as np
 from scipy.stats import norm
 from mpl_toolkits.mplot3d import Axes3D
 
+
 def genDividable2d(begin, end, bias, size):
-    
+
     base = (begin + end) / 2
-    
+
     mean1 = [base, base*(bias)]
     mean2 = [base, base*(-bias)]
 
     cov = [[1, 0], [0, 0.1]]
     negones = -1*np.ones(size)
-    
+
     data1 = np.random.multivariate_normal(mean1, cov, size)
     data2 = np.random.multivariate_normal(mean2, cov, size)
-  
-    return np.stack((negones, data1[:, 0], data1[:, 1]),axis=-1), np.stack((negones, data2[:, 0], data2[:, 1]),axis=-1)
+
+    return np.stack((negones, data1[:, 0], data1[:, 1]), axis=-1), np.stack((negones, data2[:, 0], data2[:, 1]), axis=-1)
+
 
 def genUndividable2d(begin, end, size):
     base = (begin + end) / 2
     k = rnd.uniform(0, 1)
-    
+
     mean = [base, base*k]
 
     cov = [[1, 0], [0, 0.1]]
     negones = -1*np.ones(size)
-    
+
     data1 = np.random.multivariate_normal(mean, cov, size)
     data2 = np.random.multivariate_normal(mean, cov, size)
-    return np.stack((negones, data1[:, 0], data1[:, 1]),axis=-1), np.stack((negones, data2[:, 0], data2[:, 1]),axis=-1)
+    return np.stack((negones, data1[:, 0], data1[:, 1]), axis=-1), np.stack((negones, data2[:, 0], data2[:, 1]), axis=-1)
+
 
 def genDividable3d(begin, end, bias, size):
-    
+
     base = (begin + end) / 2
-    
+
     mean1 = [base, base, base*(bias)]
     mean2 = [base, base, base*(-bias)]
 
     cov = [[1, 0, 0], [0, 1, 0], [0, 0, 0.1]]
     negones = -1*np.ones(size)
-    
+
     data1 = np.random.multivariate_normal(mean1, cov, size)
     data2 = np.random.multivariate_normal(mean2, cov, size)
-  
-    return (np.stack((negones, data1[:, 0], data1[:, 1], data1[:, 2]),axis=-1), 
-           np.stack((negones, data2[:, 0], data2[:, 1], data2[:, 2]),axis=-1))
 
-            
+    return (np.stack((negones, data1[:, 0], data1[:, 1], data1[:, 2]), axis=-1),
+            np.stack((negones, data2[:, 0], data2[:, 1], data2[:, 2]), axis=-1))
+
+
 def linearize(w):
     def lin(x):
         return -x*w[1]/w[2] + w[0]/w[2]
     return lin
 
 
-    
-
-
-# In[9]:
+# In[154]:
 
 
 size = 50
 q = 5
 eps = 0.1
+
 data = []
+data1 = []
+data2 = []
 
 y1 = -1 * np.ones(size)
 y2 = np.ones(size)
@@ -196,41 +203,86 @@ y2t = np.ones(size * (q - 1))
 yt = np.concatenate((y1t, y2t))
 
 for i in range(0, q):
-    d1, d2 = genDividable2d(0, 1, 2, size)
-    data.append([d1, d2])
+    d1, d2 = genDividable2d(0, 1, 1.2, size)
+    data1.append(d1)
+    data2.append(d2)
 
-    
-data = np.array(data)
 
-w =  [0, 1, 1]
-err = 0
+data1 = np.array(data1)
+data2 = np.array(data2)
 
-for i in range(0, q):    
-    d1 = data[i][0]
-    d2 = data[i][1]
+beg_w = [0, 1, 1]
+err_1 = 0
+err_2 = 0
+
+for i in range(0, q):
+    d1 = data1[i]
+    d2 = data2[i]
     plt.figure(1)
-    plt.plot(d1[:, 1], d1[:, 2],'o', color=(i * 1/q, i * 1/q, i * 1/q))
-    plt.plot(d2[:, 1], d2[:, 2],'o', color=(i * 1/q, i * 1/q, i * 1/q))
-    
-    x = np.concatenate((d1, d2), axis=0)     
-    data_t = np.delete(data, i, 0)
-    x1_t = data_t[:, 0]
-    x2_t = data_t[:, 1]
-    
-    xt = np.concatenate( (x1_t, x2_t), axis=0)
-    
-    gd = GradDescenter(x, y, w, 3)
+    plt.plot(d1[:, 1], d1[:, 2], 'o', color=(i * 1/q, i * 1/q, i * 1/q))
+    plt.plot(d2[:, 1], d2[:, 2], 'o', color=(i * 1/q, i * 1/q, i * 1/q))
+
+    x = np.concatenate((d1, d2), axis=0)
+
+    data1_t = np.delete(data1, i, 0)
+    data2_t = np.delete(data2, i, 0)
+    x1_t = []
+    x2_t = []
+    xt = []
+    for row in data1_t:
+        for j in row:
+            x1_t.append(j)
+
+    for row in data2_t:
+        for j in row:
+            x2_t.append(j)
+
+    for j in x1_t:
+        xt.append(j)
+
+    for j in x2_t:
+        xt.append(j)
+
+    x1_t = np.array(x1_t)
+    x2_t = np.array(x2_t)
+
+    xt = np.array(xt)
+
+    gd = GradDescenter(xt, yt, beg_w, 3)
     gd.train(0.1)
-    err += gd.validate(x, y)
-    
-err = 1/q * err    
-print(err)    
+
+    lin = linearize(gd.w)
+
+    for j in range(0, len(y)):
+        if (lin(x[j][1]) < x[j][2]) and (y[j] == 1):
+            err_1 += 1
+        elif (lin(x[j][1]) > x[j][2]) and (y[j] == -1):
+            err_1 += 1
+
+    train_set = np.array([{'x': x1_t[:, 1:], 'y': y1t},
+                          {'x': x2_t[:, 1:], 'y': y2t}])
+    lda = LdaClassifier(0.1, train_set)
+    lda.train()
+    w = np.transpose(lda.w)[0]
+    c = lda.c[0, 0]
+    for j in range(0, len(y)):
+        t = x[j][1]
+        val = -t*w[0, 0]/w[0, 1] + c/w[0, 1]
+        if (val < x[j][2]) and (y[j] == 1):
+            err_2 += 1
+        elif (val > x[j][2]) and (y[j] == -1):
+            err_2 += 1
+
+err_1 = 1/q * err_1
+err_2 = 1/q * err_2
+print(err_1)
+print(err_2)
 
 
-# In[11]:
+# In[29]:
 
 
-size = 100
+size = 50
 data1, data2 = genDividable2d(0, 1, 1.5, size)
 
 y1 = -1 * np.ones(size)
@@ -239,8 +291,8 @@ y2 = np.ones(size)
 x1 = data1[:, 1:]
 x2 = data2[:, 1:]
 
-train_set = np.array([{'x':x1, 'y': y1}, {'x': x2, 'y': y2}])
-lda = LdaClassifier(0.1,train_set)
+train_set = np.array([{'x': x1, 'y': y1}, {'x': x2, 'y': y2}])
+lda = LdaClassifier(0.1, train_set)
 lda.train()
 
 w = np.transpose(lda.w)[0]
@@ -249,15 +301,15 @@ c = lda.c[0, 0]
 xs = np.arange(-5, 5, 0.01)
 ys = [-x*w[0, 0]/w[0, 1] + c/w[0, 1] for x in xs]
 plt.figure(2)
-plt.plot(xs, ys)
 plt.plot(xs, ys, 'g')
 
 plt.plot(data1[:, 1], data1[:, 2], 'ro')
 plt.plot(data2[:, 1], data2[:, 2], 'bo')
 plt.show()
+print()
 
 
-# In[12]:
+# In[157]:
 
 
 size = 25
@@ -268,34 +320,34 @@ y2 = np.ones(size)
 
 y = np.concatenate((y1, y2))
 x = np.concatenate((data1, data2))
-xs = np.arange(-5, 5, 0.01)  
+xs = np.arange(-5, 10, 5)
 
-w =  [0, 1, 1, 0]
+w = [0, 1, 1, 0]
 gd = GradDescenter(x, y, w, 4)
 gd.train(0.1)
 
 x1 = data1[:, 1:]
 x2 = data2[:, 1:]
 
-train_set = np.array([{'x':x1, 'y': y1}, {'x': x2, 'y': y2}])
-lda = LdaClassifier(0.1,train_set)
+train_set = np.array([{'x': x1, 'y': y1}, {'x': x2, 'y': y2}])
+lda = LdaClassifier(0.1, train_set)
 lda.train()
 lw = np.transpose(lda.w)
 
 xv, yv = np.meshgrid(xs, xs)
 zvg = -1/gd.w[3] * (-gd.w[1]*xv - gd.w[2]*yv + gd.w[0])
-zvl = -1/lw[0, 2] * (-lw[0,0]*xv - lw[0, 1]*yv + lda.c[0, 0])
+zvl = -1/lw[0, 2] * (-lw[0, 0]*xv - lw[0, 1]*yv + lda.c[0, 0])
 
 fig = plt.figure(3)
 
 ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(xv, yv, zvg, color='g')
 ax.plot_surface(xv, yv, zvl, color='y')
-ax.scatter(data1[:, 1],data1[:,2],data1[:,3], color='r')
-ax.scatter(data2[:, 1],data2[:,2],data2[:,3], color='b')
+ax.scatter(data1[:, 1], data1[:, 2], data1[:, 3], color='r')
+ax.scatter(data2[:, 1], data2[:, 2], data2[:, 3], color='b')
 
 
-# In[149]:
+# In[14]:
 
 
 size = 100
@@ -311,21 +363,21 @@ x = np.concatenate((data1, data2))
 x1 = data1[:, 1:]
 x2 = data2[:, 1:]
 
-train_set = np.array([{'x':x1, 'y': y1}, {'x': x2, 'y': y2}])
-lda = LdaClassifier(0.1,train_set)
+train_set = np.array([{'x': x1, 'y': y1}, {'x': x2, 'y': y2}])
+lda = LdaClassifier(0.1, train_set)
 lda.train()
 lw = np.transpose(lda.w)
 
-w =  [0, 1, 1]
+w = [0, 1, 1]
 gd = GradDescenter(x, y, w, 3)
 gd.train(0.1)
 lin = linearize(gd.w)
 
-xs = np.arange(-5, 5, 0.01)  
-ysg = [lin(x) for x in xs]    
+xs = np.arange(-5, 5, 0.01)
+ysg = [lin(x) for x in xs]
 
 lw = np.transpose(lda.w)
-ysl = [ -x*lw[0, 0]/lw[0, 1] + lda.c[0,0]/lw[0, 1] for x in xs]
+ysl = [-x*lw[0, 0]/lw[0, 1] + lda.c[0, 0]/lw[0, 1] for x in xs]
 plt.figure(4)
 plt.plot(xs, ysg, 'g')
 plt.plot(xs, ysl, 'y')
--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[19]:
+# In[4]:
 
 
 N = 10
@@ -35,36 +35,39 @@ var_1 =  sorted(data_1)
 var_2 = sorted(data_2)
 
 
-# In[116]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-mean_1 = np.sum(data_1)/len(data_1) 
-mean_2 = np.sum(data_1)/len(data_2)
-
-
-# In[50]:
-
-
+from scipy import stats
 median_ranks = []
 for i in range(0, len(data_1)):
     median_ranks.append( ((i + 1) - 0.3 ) / (len(data_1) + 0.4))
+    
 
 
-# In[108]:
+# In[5]:
 
 
-from scipy import stats
+print('mean_1: {}, mean_2: {}'.format(np.mean(data_1), np.mean(data_2)))
+t, pval = stats.ttest_ind(var_1,var_2)
+print('same with pval: {}'.format(pval))
+
+
+# In[10]:
+
 
 #y = a + bx
-def regression(x, y, N):    
+def regression(x, y):    
     num = np.sum( (x - np.mean(x))*(y - np.mean(y)))
     den = np.sum( (x - np.mean(x))**2)
     b = num / den
     a = np.mean(y) - b * np.mean(x)
     return a, b
+
+def lin(a, b):
+    def f(x):
+        return a + b*x
+    return np.vectorize(f)
 
 x_1 = np.log(np.array(var_1))
 x_2 = np.log(np.array(var_2))
@@ -73,17 +76,28 @@ y = np.array(median_ranks)
 y = np.log(np.log(1/(1 - y)))
 
 
-c_1, beta_1 = regression(x_1, y, N)
-c_2, beta_2 = regression(x_2, y, N)
+c_1, beta_1 = regression(x_1, y)
+c_2, beta_2 = regression(x_2, y)
 
 nu_1 = np.exp(-c_1/beta_1)
 nu_2 = np.exp(-c_2/beta_2)
 
 print('beta_1: {}, nu_1: {}'.format(beta_1, nu_1))
 print('beta_2: {}, nu_2: {}'.format(beta_2, nu_2))
+lin_1 = lin(c_1, beta_1)
+lin_2 = lin(c_2, beta_2)
+
+plt.figure(0)
+plt.plot(x_1, y, 'ro')
+plt.plot(x_2, y, 'bo')
+
+plt.plot(x_1,lin_1(x_1), color='r')
+plt.plot(x_2, lin_2(x_2), color='b')
+plt.grid()
+plt.show()
 
 
-# In[122]:
+# In[8]:
 
 
 def getR(beta, nu):
@@ -94,21 +108,21 @@ def getR(beta, nu):
 R_1 = getR(beta_1, nu_1)
 R_2 = getR(beta_2, nu_2)
 
-points = np.linspace(0, 2000000, num=10000000)
+points = np.linspace(0, 2000000, num=2000000)
 plt.plot(points, R_1(points))
 plt.plot(points, R_2(points))
+
 plt.grid()
-plt.legend(['Выборка 1', 'B'])
-plt.axis(style='sci')
+plt.legend(['Выборка 1', 'Bыборка 2'])
+plt.ticklabel_format(axis='x', style='sci')
 plt.show()
 
 
-# In[121]:
+# In[9]:
 
 
-part = 0.1
-t_1 = nu_1 * math.log(-np.log(part), beta) / 100
-t_2 = nu_2 * math.log(-np.log(part), beta)/100
-print(t_1)
-print(t_2)
+part = 0.99
+t_1 = nu_1 * np.power(-np.log(part), 1/beta_1) / 100
+t_2 = nu_2 * np.power(-np.log(part), 1/beta_2) / 100
+print('t1: {}, t2: {}'.format(t_1/360, t_2/360))
 
